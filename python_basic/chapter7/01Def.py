7a57abf2ffb98b685d22bd4f379243b56129f6d6
Huajieyu001 <331173500@qq.com>
2026-02-19 21:12:37 +0800

    定义方法，实例化

--- python_basic/chapter7/01Def.py
+++ python_basic/chapter7/01Def.py
@@ -26,3 +26,18 @@ class Person:
 
     def toString(self):
         return str(self.name) + "," + str(self.age) + "," + str(self.gender)
+
+# 创建实例
+obj1 = Person("zhangsan", 21, "Male")
+obj2 = Person("lisi", 22, "Female")
+
+print(obj1)
+print(obj1.toString())
+print(type(obj1))
+print(obj1.getName())
+print(obj1.getAge())
+print(obj1.getGender())
+# 获取实例所有属性（dict）
+print(obj1.__dict__)
+# 获取类的所有属性
+print(Person.__dict__)
