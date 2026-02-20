4d9ac57fa6d2fa7b2df4b03dbf6008ae2f97d2e0
Huajieyu001 <331173500@qq.com>
2026-02-20 18:32:24 +0800

    带参装饰器和多装饰器

--- python_basic/chapter8/10decorator.py
+++ python_basic/chapter8/10decorator.py
@@ -16,6 +16,7 @@ def say_hello(caller):
 
     return wrapper
 
+
 # 手动装饰
 add_pro = say_hello(add)
 
@@ -32,4 +33,59 @@ def sub(a, b):
     print('start calculating')
     return a - b
 
+
 print(sub(1, 2))
+
+
+# 自定义一个带参数的装饰器
+def say_hello_adv(info):
+    def outer(caller):
+        def wrapper(*args, **kwargs):
+            print(info)
+            return caller(*args, **kwargs)
+
+        return wrapper
+
+    return outer
+
+
+@say_hello_adv('starting....')
+def mul(a, b):
+    print('start calculating')
+    return a * b
+
+
+print(mul(12, 20))
+
+print('-' * 50)
+
+def test1(caller):
+    print('test1 working')
+
+    def wrapper(*args, **kwargs):
+        print('test1 decorator')
+        return caller(*args, **kwargs)
+
+    return wrapper
+
+
+def test2(caller):
+    print('test2 working')
+
+    def wrapper(*args, **kwargs):
+        print('test2 decorator')
+        return caller(*args, **kwargs)
+
+    return wrapper
+
+
+# 多个装饰器一起使用
+# 会从下往上依次包装（从近到远）
+@test1
+@test2
+def div(a, b):
+    print('start calculating')
+    return a / b
+
+
+div(5, 2)
