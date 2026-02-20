38166ef094a37f96d1459fba8ba7b9d706b270b7
Huajieyu001 <331173500@qq.com>
2026-02-20 17:30:19 +0800

    手动装饰和自动装饰

--- python_basic/chapter8/10decorator.py
+++ python_basic/chapter8/10decorator.py
@@ -9,18 +9,27 @@ def add_mul(a, b, c):
 
 
 # 自定义一个装饰器
-def say_hello(caller, info):
+def say_hello(caller):
     def wrapper(*args, **kwargs):
-        print(info)
+        print('你好啊')
         return caller(*args, **kwargs)
 
     return wrapper
 
-
-add_pro = say_hello(add, '你好啊')
+# 手动装饰
+add_pro = say_hello(add)
 
 # print(add(1, 2))
 
 print(add_pro(1, 2))
 
 print(add_mul(1, 2, 3))
+
+
+# 自动装饰
+@say_hello
+def sub(a, b):
+    print('start calculating')
+    return a - b
+
+print(sub(1, 2))
