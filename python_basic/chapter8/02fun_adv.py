48b4159a7563b36cb5ec35aac188e1f62bf3075d
Huajieyu001 <331173500@qq.com>
2026-02-20 14:13:27 +0800

    条件表达式和lambda表达式（匿名函数）

--- python_basic/chapter8/02fun_adv.py
+++ python_basic/chapter8/02fun_adv.py
@@ -2,6 +2,7 @@
 def multiResult(x, y):
     return x + y, x - y
 
+
 print(multiResult(1, 2))
 
 # 接收多返回值
@@ -10,9 +11,12 @@ print(a, b)
 
 # 打包
 print('*' * 30)
+
+
 def pac(*args, **kwargs):
     print(args)
     print(kwargs)
+
 
 pac(1, 2, 3, k1=111, k2=222)
 
@@ -22,3 +26,9 @@ pac(*args, **kargs)
 
 print('=' * 30)
 print(*args)
+
+#
+# 条件表达式(相当于三目运算符)          值1 if 布尔表达式 else 值2
+a = 1
+b = 2
+print('aaa' if a > b else 'bbb')
